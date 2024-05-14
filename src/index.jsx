f552522f2069e453c680991c84b6d9a7ed62b7a2
Andrej-Nil <and.kucheroff@yandex.ru>
2024-05-14 10:24:23 +0500

    комит перед началом разработки бекенда

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
 import ReactDOM from 'react-dom/client'
 import './index.scss';
-// import App from './App.jsx'
-// import {BrowserRouter} from "react-router-dom";
+import App from './App.jsx'
+import {BrowserRouter} from "react-router-dom";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  //   <BrowserRouter>
-      <div>test</div>
-        // {/*<App />*/}
-    // </BrowserRouter>
+    <BrowserRouter>
+
+         <App />
+   </BrowserRouter>
   // </React.StrictMode>,
 )
